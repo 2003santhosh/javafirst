4a5ee8ed16eb7a582af74ed5155702a203f794b8
2003santhosh <116159895+2003santhosh@users.noreply.github.com>
2024-09-27 00:09:56 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         int x = 100;
+        int y = 120;
         System.out.println(x);
         System.out.println("Hello, World!");
     }
