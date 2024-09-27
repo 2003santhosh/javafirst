fe3fe4a6e8ad3e0db94da2bb03df0d664ab75f92
2003santhosh <svsanthoshkrishnan@gmail.com>
2024-09-27 11:10:36 +0530

    hi

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        int x = 100;
-        int y = 120;
+        int x = 20, y =100;
+        
+        System.out.println(y);
         System.out.println(x);
         System.out.println("Hello, World!");
     }
